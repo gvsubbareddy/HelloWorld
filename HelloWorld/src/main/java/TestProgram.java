2a9a68d71627de7cbecb8dcd118db70aa8001eed
gvsubbareddy <subbareddy.guvvala@gmail.com>
2014-06-22 12:05:08 +0530

    Commit Test
    Signed-off-by: gvsubbareddy <subbareddy.guvvala@gmail.com>

---
+++ HelloWorld/src/main/java/TestProgram.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+
+/**
+ * @author vijay
+ *
+ */
+public class TestProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
